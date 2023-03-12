c4b689e7548f985df7d542abdb0022c807bb208b
Станислав <113885026+StanislavKon@users.noreply.github.com>
2023-03-12 18:54:01 +0300

    hw1.8

---
+++ HW1/HygieneItems.java
@@ -0,0 +1,16 @@
+package Homeworks.HW_1;
+
+public class HygieneItems extends Goods{
+    Integer numberOfPack;
+
+    public HygieneItems(String name, Double price, Integer amount, String unit, Integer numberOfPack) {
+        super(name, price, amount, unit);
+        this.numberOfPack = numberOfPack;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder().append(super.toString())
+                .append(String.format("\tКоличество штук в упаковке: %d;\n", numberOfPack)).toString();
+    }
+}
