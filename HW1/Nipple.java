7b512ac5e14d80d617ca4b10add3a56a4fcc291c
Станислав <113885026+StanislavKon@users.noreply.github.com>
2023-03-12 18:59:48 +0300

    hw1.12

---
+++ HW1/Nipple.java
@@ -0,0 +1,9 @@
+package Homeworks.HW_1;
+
+public class Nipple extends ChildrenGoods{
+
+    public Nipple(String name, Double price, Integer amount, String unit,
+                  Integer minAge, String hypoallergenic) {
+        super(name, price, amount, unit, minAge, hypoallergenic);
+    }
+}
