748e347ec93f9e28c6b3e17caf9929d8a3a81c14
Станислав <113885026+StanislavKon@users.noreply.github.com>
2023-03-12 18:56:53 +0300

    hw1.10

---
+++ HW1/Masks.java
@@ -0,0 +1,7 @@
+package Homeworks.HW_1;
+
+public class Masks extends HygieneItems{
+    public Masks(String name, Double price, Integer amount, String unit, Integer numberOfPack) {
+        super(name, price, amount, unit, numberOfPack);
+    }
+}
