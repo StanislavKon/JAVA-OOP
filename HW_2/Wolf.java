41b51c06fa5b711595f044877f807718d0a7bcba
Станислав <113885026+StanislavKon@users.noreply.github.com>
2023-03-12 19:33:43 +0300

    hw2.15

---
+++ HW_2/Wolf.java
@@ -0,0 +1,26 @@
+package Homeworks.HW_2;
+
+public class Wolf extends WildAnimal implements MakesSound{
+    private String leader;
+
+    public Wolf(double growth, double weight, String eyeColor, String habitat,
+                String dataBirth, String leader) {
+        super(growth, weight, eyeColor, habitat, dataBirth);
+        this.leader = leader;
+    }
+
+    public String getLeader() {
+        return leader;
+    }
+
+    @Override
+    public void makesSound() {
+        System.out.println("Уyyyyyyyyy");
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder().append(super.toString())
+                .append(String.format("\tВожак стаи: %s;\n", leader)).toString();
+    }
+}
