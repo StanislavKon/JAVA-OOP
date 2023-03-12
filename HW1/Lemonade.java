2af3019dd02047c6cc6ca88f2962301a946464a4
Станислав <113885026+StanislavKon@users.noreply.github.com>
2023-03-12 18:55:37 +0300

    hw1.9

---
+++ HW1/Lemonade.java
@@ -0,0 +1,8 @@
+package Homeworks.HW_1;
+
+public class Lemonade extends Drinkables{
+    public Lemonade(String name, Double price, Integer amount, String unit, Double volume) {
+        super(name, price, amount, unit, volume);
+    }
+
+}
