156d211aa1270e9151d517ee6b2d4d771dea0b65
Станислав <113885026+StanislavKon@users.noreply.github.com>
2023-03-12 18:43:21 +0300

    hw1.6

---
+++ HW1/Food.java
@@ -0,0 +1,17 @@
+package Homeworks.HW_1;
+
+public class Food extends Goods {
+    private String expirationDate;
+
+    public Food(String name, Double price, Integer amount, String unit, String expirationDate) {
+        super(name, price, amount, unit);
+        this.expirationDate = expirationDate;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder().append(super.toString())
+                .append(String.format("\tСрок годности: %s;\n", expirationDate)).toString();
+
+    }
+}
