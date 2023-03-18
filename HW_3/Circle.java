f0305d3f974011307f09228a23ef8e88b5b0fd0e
Станислав <113885026+StanislavKon@users.noreply.github.com>
2023-03-18 21:26:37 +0300

    hw3.1

---
+++ HW_3/Circle.java
@@ -0,0 +1,28 @@
+package Homeworks.HW_3;
+
+public class Circle extends Figure{
+    protected double radius;
+
+    public Circle(double radius) throws Exception {
+        this.radius = radius;
+        if (radius <= 0){
+            throw new Exception("Радиус должен быть больше 0!");
+        }
+
+    }
+
+    @Override
+    public double squareFigure() {
+        return Math.PI * this.radius * this.radius;
+    }
+
+    public double circumference() {
+        return 2 * Math.PI * this.radius;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder().append(super.toString())
+                .append(String.format("\n\tДлина окружности, см: %.2f;\n", circumference())).toString();
+    }
+}
